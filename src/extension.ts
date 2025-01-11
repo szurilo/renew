ed5f21acb0c52a4f0f0a70afd913f5d38fd4e3eb
József Tar <szurilo@gmail.com>
2025-01-12 00:23:01 +0100

    - feature: set image and text gen limit, improve logging - fix: image converter resize insufficiency

--- src/extension.ts
+++ src/extension.ts
@@ -7,20 +7,22 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { parseDocument } from "htmlparser2";
 import serialize from "dom-serializer";
-import ky, { Input } from "ky";
 import sharp from "sharp";
+import { randomUUID } from "crypto";
 
+const PNG_MAX_FILE_SIZE = 4 * 1024 * 1024; // 4MB
+const imageGenLimit = 0;
+const textGenLimit = 10;
 let openai: OpenAI;
+let imageGenerations: number;
+let textGenerations: number;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	const envPath = path.join(context.extensionPath, '.env');
 	dotenv.config({ path: envPath });
 	openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
-	// This line of code will only be executed once when your extension is activated
-	console.log('Renew: Congratulations, your extension is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -28,6 +30,8 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('renew.redesign', async () => {
 		// The code you place here will be executed every time your command is executed
 		vscode.window.showInformationMessage('Renew: Redesign is in progress...');
+		imageGenerations = 0;
+		textGenerations = 0;
 		await replaceContentInFiles();
 		vscode.window.showInformationMessage('Renew: Redesign is complete');
 	});
@@ -35,38 +39,39 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 const replaceContentInFiles = async () => {
-
 	const files = await vscode.workspace.findFiles('**/*.html', '**/node_modules/**');
 
 	for (const file of files) {
 		const document = await vscode.workspace.openTextDocument(file);
-		const text = document.getText();
+		const documentText = document.getText();
 
-		// replace images
-		const imgTags = text.match(/<img[^>]+src=["']([^"']+)["'][^>]*>/g);
-		const imageUrls = imgTags?.map(tag => tag.match(/src=["']([^"']+)["']/)[1]) || [];
-		for (const imageUrl of imageUrls) {
-			const imageFilePath = await getFilePathFromWorkspace(imageUrl);
-			if (imageFilePath) {
-				await replaceImage(imageFilePath);
-			}
+		if (imageGenerations < imageGenLimit) {
+			await replaceImages(documentText);
 		}
-
-		const updatedText = await replaceTextInHtml(text);
-
-		const edit = new vscode.WorkspaceEdit();
-		const fullRange = new vscode.Range(
-			document.positionAt(0),
-			document.positionAt(text.length)
-		);
-		edit.replace(file, fullRange, updatedText);
-		await vscode.workspace.applyEdit(edit);
-
+		if (textGenerations < textGenLimit) {
+			await replaceTexts(documentText, document, file);
+		}
 	}
-	return;
 };
 
-export async function getFilePathFromWorkspace(fileName: string): Promise<string | undefined> {
+const replaceImages = async (text: string) => {
+	const imgTags = text.match(/<img[^>]+src=["']([^"']+)["'][^>]*>/g);
+	const imageUrls = imgTags?.map(tag => tag.match(/src=["']([^"']+)["']/)?.[1]) || [];
+	for (const imageUrl of imageUrls) {
+		const imageFilePath = await getFilePathFromWorkspace(imageUrl);
+		if (imageFilePath) {
+			if (imageGenerations >= imageGenLimit) {
+				vscode.window.showInformationMessage('Renew: Image generation limit reached');
+				console.log("Image generation limit reached");
+				return;
+			}
+			imageGenerations++;
+			await replaceImage(imageFilePath);
+		}
+	}
+};
+
+const getFilePathFromWorkspace = async (fileName: string | undefined): Promise<string | undefined> => {
 	const workspaceFolders = vscode.workspace.workspaceFolders;
 
 	if (!workspaceFolders) {
@@ -84,48 +89,29 @@ export async function getFilePathFromWorkspace(fileName: string): Promise<string
 		vscode.window.showErrorMessage(`Renew: File "${fileName}" not found in workspace`);
 		return undefined;
 	}
-}
+};
 
 const replaceImage = async (imageFilePath: string) => {
-
 	const pngImageFilePath = replaceFileExtensionToPng(imageFilePath);
 	await convertImageToPng(imageFilePath, pngImageFilePath);
 
+	console.log("Calling imageGeneration model");
 	const response = await openai.images.createVariation({
 		model: "dall-e-2",
 		image: fs.createReadStream(pngImageFilePath),
 		n: 1,
 		size: "256x256"
 	});
-
-	const resp = await ky.get(response.data[0].url as Input);
+	const { default: ky } = await import('ky');
+	const resp = await ky.get(response.data[0].url!); // Add non-null assertion since we know URL exists
 	const newImage = await resp.arrayBuffer();
 
 	const buffer = Buffer.from(newImage);
 	fs.writeFileSync(pngImageFilePath, buffer);
+	console.log("Replacement image saved to " + pngImageFilePath);
 };
 
-async function convertImageToPng(inputPath: string, outputPath: string) {
-	try {
-		await sharp(inputPath)
-			.png() // Convert to PNG
-			.toBuffer({ resolveWithObject: true })
-			.then(({ data, info }) => {
-				if (info.size > 4 * 1024 * 1024) { // Check size
-					return sharp(data)
-						.resize({ width: Math.round(info.width * 0.9) }) // Resize proportionally
-						.toFile(outputPath);
-				} else {
-					fs.writeFileSync(outputPath, data);
-				}
-			});
-		console.log(`Image converted and saved to ${outputPath}`);
-	} catch (error) {
-		console.error('Error converting image:', error);
-	}
-}
-
-function replaceFileExtensionToPng(filePath: string): string {
+const replaceFileExtensionToPng = (filePath: string): string => {
 	const parsedPath = path.parse(filePath);
 	const pngPath = path.format({
 		...parsedPath,
@@ -133,43 +119,86 @@ function replaceFileExtensionToPng(filePath: string): string {
 		ext: '.png'
 	});
 	return pngPath;
-}
+};
 
-async function replaceTextInHtml(html: string): Promise<string> {
-	const dom = parseDocument(html);
-
-	const traverseTextNodes = async (node: any) => {
-		if (node.type === 'text' && node.data.trim()) {
-			const originalText = node.data.trim();
-			console.log(originalText);
-			const replacementText = await updateText(originalText);
-			console.log(replacementText);
-			node.data = node.data?.replace(originalText, replacementText);
-		}
-		if (node.children) {
-			node.children.forEach(traverseTextNodes);
-		}
-	};
-
-	dom.children.forEach(traverseTextNodes);
-
-	return serialize(dom);
-}
-
-const updateText = async (text: string): Promise<any> => {
-	if (text) {
-		console.log("Calling textGeneration model");
-		const completion = await openai.chat.completions.create({
-			model: "gpt-4o-mini",
-			store: true,
-			messages: [{ role: "system", content: "You are just rephrasing the given input, but make sure the input is human readable text, like sentences or phrases. Your answers are always less than 100 words." },
-			{ role: "user", content: text }
-			]
-		});
-		return completion.choices[0].message.content;
-	} else {
-		return "";
+const convertImageToPng = async (inputPath: string, outputPath: string) => {
+	try {
+		await sharp(inputPath)
+			.png() // Convert to PNG
+			.toBuffer({ resolveWithObject: true })
+			.then(async ({ data, info }) => {
+				let currentData = data;
+				let currentInfo = info;
+				while (currentInfo.size > PNG_MAX_FILE_SIZE) { // Check size
+					await sharp(currentData)
+						.resize({ width: Math.round(currentInfo.width * 0.9) }) // Resize proportionally
+						.toBuffer({ resolveWithObject: true })
+						.then(({ data: resizedData, info: resizedInfo }) => {
+							currentData = resizedData;
+							currentInfo = resizedInfo;
+						});
+				}
+				fs.writeFileSync(outputPath, currentData);
+			});
+		console.log(`Image converted and saved to ${outputPath}`);
+	} catch (error) {
+		console.error('Error converting image:', error);
 	}
+};
+
+const replaceTexts = async (documentText: string, document: vscode.TextDocument, file: vscode.Uri) => {
+	const updatedText = await replaceTextInHtml(documentText);
+
+	const edit = new vscode.WorkspaceEdit();
+	const fullRange = new vscode.Range(
+		document.positionAt(0),
+		document.positionAt(documentText.length)
+	);
+	edit.replace(file, fullRange, updatedText);
+	await vscode.workspace.applyEdit(edit);
+};
+
+const replaceTextInHtml = async (html: string): Promise<string> => {
+	const dom = parseDocument(html);
+	for (const child of dom.children) {
+		await traverseTextNodes(child);
+	}
+	return serialize(dom);
+};
+
+const traverseTextNodes = async (node: any): Promise<void> => {
+	if (node.type === 'text' && node.data.trim()) {
+
+		if (textGenerations >= textGenLimit) {
+			vscode.window.showInformationMessage('Renew: Text generation limit reached');
+			console.log("Text generation limit reached");
+			return;
+		}
+		textGenerations++;
+
+		const originalText = node.data.trim();
+		const uuid = randomUUID();
+		console.log("originalText_" + uuid + ": " + originalText);
+		const replacementText = await replaceText(originalText);
+		console.log("replacementText_" + uuid + ": " + replacementText);
+		node.data = node.data?.replace(originalText, replacementText);
+	}
+	if (node.children) {
+		for (const child of node.children) {
+			await traverseTextNodes(child);
+		}
+	}
+};
+
+const replaceText = async (text: string): Promise<any> => {
+	const completion = await openai.chat.completions.create({
+		model: "gpt-4o-mini",
+		store: true,
+		messages: [{ role: "system", content: "You are just rephrasing the given input, but make sure the input is human readable text, like sentences or phrases. Your answers are always less than 100 words." },
+		{ role: "user", content: text }
+		]
+	});
+	return completion.choices[0].message.content;
 };
 
 export function deactivate() { }
